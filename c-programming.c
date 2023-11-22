b3de94c5b86b8678e9e58d5abb1a3fea408f048c
Janit Yadav <janityadav16@gmail.com>
2023-11-22 22:20:02 +0530

    to pratice c code

--- c-programming.c
+++ c-programming.c
@@ -1,1 +1,12 @@
-
+int main()
+{
+int ar[100],i,n;
+printf("\n enter the size of the array");
+scanf("%d",&n);
+printf("enter the value in the array \n");
+for(i=0;i<n;i++)
+scanf("%d",&ar[i]);
+printf("\n the element of the array are\n");
+for(i=0;i<n;i++)
+printf("%d\t",ar[i]);
+}
